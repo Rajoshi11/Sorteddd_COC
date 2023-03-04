f091c28be7487a09ce72cac11bce93ac1ccec840
Rajoshi11 <ruju111202@gmail.com>
2023-03-04 14:30:07 +0530

    Landing page changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
+import React, { useState, useEffect } from 'react';
 import Spline from "@splinetool/react-spline";
 import styled from "styled-components";
-import "./styles.css"
+import "./styles.css";
+import TrackVisibility from 'react-on-screen';
 
 export default function App() {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [index, setIndex] = useState(1);
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const toRotate = [ "Let's catch the match!" ];
+  const period = 1500;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
   return (
     <Wrapper>
       <Spline scene="https://prod.spline.design/0au9Qy8Tn2a5MENt/scene.splinecode" />
@@ -30,13 +72,17 @@ export default function App() {
             <button>Get Started</button>
           </li>
         </Menu>
-        <h1>Let's catch that Match!</h1>
-        <p>
-          Bring your team together and build your community by using our
-          cross-platform app that lets you collaborate via chat, voice and by
-          sharing and storing unlimited media files. A world of topics is
-          waiting for you. Join the private beta.
-        </p>
+
+        <TrackVisibility>
+            {({ isVisible }) =>
+            <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+              <h1>{``} <span className="txt-rotate" dataperiod="1000" data-rotate='[ "Lets catch the match!" ]'><span className="wrap tracking-tight">{text}</span></span></h1>
+                <br></br>
+                <br></br>
+                <br></br>
+            </div>}
+          </TrackVisibility>
+          <p style={{fontSize: '30px'}}>It is never too late to fall in love!</p>
         <button>
            Download now!
         </button>
